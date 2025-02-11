0bc6cc3a3af5269a8a6c1c8df993b1be8959e93a
Alexander Kukushkin <alex@alexkuk.ru>
2025-02-11 16:41:06 +0300

    Extract humaneval with regexp

--- simple/benches/humaneval.py
+++ simple/benches/humaneval.py
@@ -1,32 +1,17 @@
 
+import re
+
 
 def humaneval_instruction(prompt):
     return """Read the following function signature and docstring, and fully implement the function described. Your response should only contain the code for this function.
 %s""" % prompt
 
 
-def humaneval_postproc_instruction(answer, entry_point):
-    return f"""From input text extract the function "{entry_point}" body lines. Respond only with function body lines, do not add ```. You response should be a substring of input text, keep the original indentation. In case the function is not found responde with empty string.
-
-Example input:
-```python
-def all_prefixes(string: str) -> List[str]:
-    ''' Return list of all prefixes from shortest to longest of the input string
-    '''
-    return [string[:i] for i in range(1, len(string) + 1)]
-```
-
-Output:
-    return [string[:i] for i in range(1, len(string) + 1)]
-
-
-----
-
-Input text is inside ```:
-
-```
-{answer}
-```"""
+def humaneval_answer_pred(answer):
+    chunks = re.findall(r"```(?:python)?(.+?)```", answer, flags=re.DOTALL)
+    if not chunks:
+        return answer
+    return chunks[0]
 
 
 async def humaneval_worker(model_client, task_item, res_item, context):
@@ -38,13 +23,7 @@ async def humaneval_worker(model_client, task_item, res_item, context):
     res_item["usage"] = response["usage"]
     res_item["model_cost"] = response["cost"]
 
-    response = await context["openrouter"](
-        model="google/gemini-flash-1.5",
-        instruction=humaneval_postproc_instruction(
-            res_item["answer"], task_item["entry_point"])
-    )
-    res_item["pred"] = response["answer"]
-    res_item["bench_cost"] = response["cost"]
+    res_item["pred"] = humaneval_answer_pred(response["answer"])
 
     response = await context["e2b"](code=f"""
 {task_item["prompt"]}
@@ -61,4 +40,4 @@ check({task_item["entry_point"]})
         not res_item["traceback"]
         and not res_item["timed_out"]
     )
-    res_item["bench_cost"] += response["cost"]
+    res_item["bench_cost"] = response["cost"]
